eef164e89dac3e9ff3aedb3b83b81e60ec613d6f
sheraz-amjad <sherazamjad933@gmail.com>
2024-08-23 16:26:18 +0500

    WhatsApp Ui Files

---
+++ lib/live.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class live extends StatefulWidget {
+  const live({super.key});
+
+  @override
+  State<live> createState() => _liveState();
+}
+
+class _liveState extends State<live> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
